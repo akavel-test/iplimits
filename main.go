7e2ee4c9d7cc7df3b28ee5ae8e98aecf8b9ad955
Mateusz Czapliński <czapkofan@gmail.com>
2023-09-15 17:14:23 +0200

    main: add TODOs - sketch of plan

--- main.go
+++ main.go
@@ -4,4 +4,12 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello iplimits")
+
+	// TODO: test setting limit for sample IP by hand via `nft` command
+	// TODO: test purging limit for sample IP by hand via `nft` command
+	// TODO: write code to set limit from Go - see if it works
+	// TODO: if os.Args[1] == 'purge' { purge_all_limits() } - see if it works
+	// TODO: pretty flags for adding limits - try to implement them incrementally:
+	// - variable IP
+	// - variable packets OR bandwidth limit
 }
