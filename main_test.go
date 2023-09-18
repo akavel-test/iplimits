b9b5fbf8769f71237dcdb24ea419c099c6a876e9
Mateusz Czapliński <czapkofan@gmail.com>
2023-09-18 18:46:19 +0200

    test: more

--- main_test.go
+++ main_test.go
@@ -44,6 +44,14 @@ func TestParseAddLimitArgs_Invalid(t *testing.T) {
 			args:          args{},
 			wantErrPrefix: "not enough arguments",
 		},
+		{
+			args:          args{"1.2.3.4"},
+			wantErrPrefix: "not enough arguments",
+		},
+		{
+			args:          args{"1.2.3.4", "100"},
+			wantErrPrefix: "not enough arguments",
+		},
 	}
 
 	for _, tt := range tests {
@@ -52,6 +60,37 @@ func TestParseAddLimitArgs_Invalid(t *testing.T) {
 		if !strings.HasPrefix(errText, tt.wantErrPrefix) {
 			t.Errorf("%q: bad error:\nwant: %s...\nhave: %s",
 				tt.args, tt.wantErrPrefix, errText)
+		}
+	}
+}
+
+func TestParseAddLimitArgs_OK(t *testing.T) {
+	type args []string
+	tests := []struct {
+		comment string
+		args    args
+		want    filterArgs
+	}{
+		{
+			comment: "",
+			args:    args{"1.2.3.4", "100", "kbps"},
+			want: filterArgs{
+				IP:        netip.MustParseAddr("1.2.3.4"),
+				RateValue: 100,
+				RateUnit:  " kbytes/second",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		have, err := parseAddLimitArgs(tt.args)
+		if err != nil {
+			t.Errorf("%q: unexpected error: %s",
+				tt.args, err)
+		}
+		if have != tt.want {
+			t.Errorf("%q: bad result:\nhave: %v\nwant: %v",
+				tt.args, have, tt.want)
 		}
 	}
 }
